0bc7c16a31ef602d3002322c2b133e87f2261911
Haider <Haiderkg78@gmail.com>
2026-08-07 23:27:15 +0530

    feat: Add CORS middleware for API and create .gitignore file

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import authRoutes from "./modules/auth/auth.route.js";
 import apiKeyRoutes from "./modules/api_key/apiKey.route.js";
 import adminRoutes from "./modules/admin/admin.route.js";
@@ -9,6 +10,12 @@ import cookieParser from "cookie-parser";
 import { errorHandler } from "./middlewares/error.middleware.js";
 
 const app = express();
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(cookieParser());
 app.get("/", (req, res) => {
   res.send("Hello World!");
